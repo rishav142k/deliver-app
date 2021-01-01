08f9e63a7c7386f83cdc35682636104ae3290cb3
Rishav Sharma <sharmarishav147@gmail.com>
2021-01-01 14:05:01 +0530

    Initial Commit

---
+++ customer/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.Index.as_view(), name = "index"),
+    path('about/', views.About.as_view(), name='about'),
+    path('order/', views.Order.as_view(), name='order'),
+    path('order-confirmation/<int:pk>/', views.OrderConfirmation.as_view(),
+        name='order-confirmation'),
+    path('order-complete/<int:pk>/', views.Order_complete.as_view(), name = "order_complete"),
+]
